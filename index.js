685a86345c7a423ef832c07fdc2d3bbe1e905e9c
uzcategui <uz.uzcategui.uz@gmail.com>
2022-05-14 20:07:43 +0200

    Primer commit reto1

---
+++ index.js
@@ -0,0 +1,22 @@
+function suma(op1, op2) {
+    return op1+op2;
+}
+
+function resta(op1, op2) {
+    return op1-op2;
+}
+
+function producto(op1, op2) {
+    return op1*op2;
+}
+
+function divicion(op1, op2) {
+    return op1/op2;
+}
+
+function cuadrado(number){
+    return number ** 2;
+}
+
+
+module.exports={suma, resta, producto,divicion};
---
+++ run.js
@@ -0,0 +1,36 @@
+let myLib = require("./index");
+
+myLib.suma(9,6)
+myLib.resta (9,6)
+myLib.producto(9,6)
+myLib.divicion(9,6)
+console.log(myLib.suma(9,6))
+console.log(myLib.resta (9,6))
+/*
+let vector = require("./libreriaVector");
+let funsiones = require("./libreriaFuncion");
+console.log(myLib.suma(5, 8));
+console.log(myLib.resta(5, 8));
+console.log(myLib.producto(5, 8));
+console.log(myLib.divicion(5, 8));
+
+let v1 = vector.crearVector(5,100);
+let v2 = vector.crearVector(5,100);
+console.log(v1);
+console.log(v2);
+console.log(vector.sumaVector(v1, v2));
+console.log(vector.productoNumeroVector(10,v1));
+console.log(vector.restaVector(v1,v2));
+console.log(vector.productoVector(v1,v2));
+
+console.log(funsiones.sumaVector1(v1,v2));
+console.log(funsiones.sumaVector2(v1,v2));
+console.log(funsiones.filtarPares(v1));
+console.log(funsiones.sumatorio(v1));
+*/
+
+
+
+
+
+
